4b7c236eb13b2893ad177b48355a6297ca00f5da
Ashley Margallo <amarie921@yahoo.com>
2018-02-19 17:26:21 -0700

    two challenges current

--- golf.rb
+++ golf.rb
@@ -3,12 +3,12 @@
 # sums the array 
 # then returns the array and the sum
 
-# def array(n)
-#   n.inject(:+)
-# end
+def array(n)
+  n.inject(:+)
+end
 
-# n = [1, 2, 3, 5]
-# puts array(n)
+n = [1, 2, 3, 5]
+puts array(n)
 
 
 #HOLE 2: Rock paper scissors
@@ -16,31 +16,34 @@
 #computer randomly chooses 
 #winner is output to the screen.
 
-
-def user_input
-  print "Input Rock, Paper, or Scissors"
-  user_input = gets.string.to_s
+def user_input 
+  puts "Input Rock, Paper, or Scissors"
+  user_input = gets.strip.to_s
   arr = ["Rock", "Paper", "Scissors"]
   comp_input = arr.sample
   case user_input
     when user_input === 'Rock' && comp_input === 'Scissors'
-      # user_input = "Rock"
-      # comp_input = "Scissors"
       puts "You win"
     when user_input === 'Paper' && comp_input === 'Rock'
-      # user_input = "Paper"
-      # comp_input = "Rock"
       puts "You win"
     when user_input === 'Scissors' && comp_input === 'Paper'
-      # user_input = "Scissors"
-      # comp_input = "Paper"
       puts "You win"
     else 
       puts "You lose"
   end
 end
 
+user_input
 
+
+# HOLE 3: FIZZBUZZ
+# For numbers 1-100 
+# print “FIZZ” if the number is divisible by 3
+# print “BUZZ” if the number is divisible by 5
+# print “FIZZBUZZ” if the number is divisible by both 3 and 5 
+# else print the number
+
+arr = ["1"..."100"]
 
 
 
